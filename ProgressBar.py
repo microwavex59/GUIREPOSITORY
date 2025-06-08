5dcad32e45df123cfae415297110792980fd4e5b
microwavex59 <microwavex59@gmail.com>
2025-06-08 15:03:26 -0500

    Add files via upload

---
+++ ProgressBar.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import *
+
+window=Tk()
+window.geometry("400x400")
+progress=Progressbar(window,orient=HORIZONTAL,length=100,mode="determinate")
+
+def progresscommand():
+    progress["value"]=25
+    window.update_idletasks()
+    time.sleep(1)
+    progress["value"]=50
+    window.update_idletasks()
+    time.sleep(1)
+    progress["value"]=75
+    window.update_idletasks()
+    time.sleep(1)
+    progress["value"]=100
+    window.update_idletasks()
+    time.sleep(1)
+
+button=Button(window,text="This is a button",command=progresscommand)
+button.pack(padx=10,pady=10)
+
+progress.pack(padx=10,pady=50)
+
+
+
+
+
+
+
+window.mainloop()
